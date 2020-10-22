49af93c05e259d58f614042a289281926a19efdc
lunacy001 <43693807+Lunacy001@users.noreply.github.com>
2020-10-22 10:37:18 +0530

    Added more utils and tests

--- src/helper/index.js
+++ src/helper/index.js
@@ -16,9 +16,42 @@ function generateRandomId() {
 async function hashPassword(password) {
   return await bcrypt.hash(password, 10);
 }
+/**
+ * 
+ * @param {string} string input that needs to be validated
+ * 
+ * @returns {boolean} returns true if valid string or false otherwise
+ */
+function validateString(string) {
+  if (!string) {
+    return false;
+  }
+  if (typeof string !== 'string') {
+    return false;
+  }
+  if (string.trim().length === 0) {
+    return false;
+  }
+
+  return true;
+}
+
+/**
+ *
+ * @param {string} input The string to be validated for valid email
+ *
+ * @returns {boolean} Whether the passed in string is a valid email or not
+ */
+function validateEmail(input) {
+  const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!input || typeof input !== 'string') return false;
+  return regex.test(String(input).toLocaleLowerCase());
+}
 
 const helpers = {
   generateRandomId,
-  hashPassword
+  hashPassword,
+  validateEmail,
+  validateString
 };
 module.exports = helpers;
--- test/helper/helper.test.js
+++ test/helper/helper.test.js
@@ -22,4 +22,40 @@ describe('generateRandomId', () => {
     expect(bcryptSpy).toHaveBeenCalledWith('abcd', 10);
     expect(hashedString).toEqual('hashedstring');
   });
+
+  describe('validateString', () => {
+    it.each([
+      ['', false],
+      ['   ', false],
+      [1234, false],
+      [null, false],
+      [undefined, false],
+      ['hello', true],
+      [`template string`, true],
+      ['    spaced string    ', true]
+    ])('.should return $expected when string is $input', (input, expected) => {
+      const testString = input;
+      const valid = helpers.validateString(testString);
+
+      expect(valid).toBe(expected);
+    });
+  });
+
+  describe('validateEmail', () => {
+    it.each([
+      ['', false],
+      [123, false],
+      ['test@.com', false],
+      ['test.com', false],
+      ['test@test.com', true]
+    ])(
+      '.should return $expected when input string is $input',
+      (input, expected) => {
+        const testString = input;
+        const valid = helpers.validateEmail(testString);
+
+        expect(valid).toBe(expected);
+      }
+    );
+  });
 });
